c93b26065b6c1afe3a1a640428a6521e933df2a5
federicosldev <sanchezlisonfederico2001@gmail.com>
2024-03-23 17:58:18 +0100

    New features

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Components/Navbar'
+import Content from './Components/Content'
+
+function App() {
+  return (
+    <div className='container'>
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <img src="/assets/logo.png" alt="imagen logo react" />        
+                <ul>
+                    <li>React Course - Project 1</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
